29a082b2a0d2fd44b6c517136adee006fc539db8
pakcdf <juseongb24@gmail.com>
2024-10-02 14:46:27 +0900

    text 주석

---
+++ 01-GNB/01/js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+  //  대상을 변수에 저장
+  const $menu = $(".gnb >li");
+  const $submenu = $(".submenu");
+  const duration = 300; //0.3초
+  // 메뉴에 마우스가 들어왔을때
+  $menu.on("mouseenter", function () {
+    // .stop() : 현재진행 중인 애니메이션 즉시 중지
+    $(this).find($submenu).stop().slideDown(duration);
+    $(this).addClass("on");
+  });
+  // 메뉴에 마우스가 나갔을때
+  $menu.on("mouseleave", function () {
+    // 테스트 주석
+    $submenu.stop().slideUp(duration);
+    $menu.removeClass("on");
+  });
+});
